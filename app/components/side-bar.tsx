93baba473cc84c56b25f173cfd6885fb906f0172
Ono Keita <oh.kitbsk.1028@icloud.com>
2024-09-01 06:00:56 +0000

    chore: Update side bar styling to fix hidden height issue

--- app/components/side-bar.tsx
+++ app/components/side-bar.tsx
@@ -7,7 +7,7 @@ import SideBarButton from './side-bar-button';
 export default function SideBar() {
   return (
     <ScrollArea className='h-[calc(100dvh-130px)]'>
-      <div className='hidden w-64 border-r lg:block'>
+      <div className='hidden h-[calc(100dvh-130px)] w-64 border-r lg:block'>
         <div className='flex flex-col space-y-1 p-4'>
           {mainTags.map((tagId) => (
             <SideBarButton key={tagId} tagId={tagId} />
